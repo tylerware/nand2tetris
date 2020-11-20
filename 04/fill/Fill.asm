b2606e9cbfd2e9115a0c657d085067db7954c2a9
Tyler Ware <tyler.ware.777@gmail.com>
2020-11-19 22:03:52 -0700

    Flashing the screen black an white on a keypress
    Well, the virtual screen that is.

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -11,4 +11,57 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+(LOOP)
+    @i
+    M=0
+
+    @KBD
+    D=M             // Store pressed key in D register
+
+    @CLEAR          // If key is not pressed goto CLEAR
+    D;JEQ
+    @FILL           // otherwise goto FILL
+    0;JEQ
+
+(CLEAR)
+    @state
+    M=0
+    @CHANGE_SCREEN
+    0;JMP
+
+(FILL)
+    @state
+    M=-1
+    @CHANGE_SCREEN
+    0;JMP
+
+(CHANGE_SCREEN)
+    @i
+    D=M
+    @8192       // (32 words) * (256 rows)
+    D=A-D       // (Total words) - (Current row)
+    @END
+    D;JEQ
+
+    @i
+    D=M
+    @SCREEN
+    D=D+A
+
+    @word
+    M=D
+    @state
+    D=M
+    @word
+    A=M
+    M=D
+
+    @i
+    M=M+1
+    @CHANGE_SCREEN
+    0;JMP
+
+
+(END)
+    @LOOP
+    0;JMP // always go back to the beginning
